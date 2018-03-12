711ed8f8051e5b6ed7b18159842690e760d88e50
yangchunliang <1711626079@qq.com>
2018-03-12 14:36:32 +0800

    修改左侧导航处样式

--- Client/src/components/LeftNav.vue
+++ Client/src/components/LeftNav.vue
@@ -1,15 +1,19 @@
 <template>
     <div class="left-nav">
+        <img class="left-header" src="../../static/img/my-header.png" alt="">
+        <div class="left-name">Younger</div>
+        <div class="left-infor">不积跬步无以至千里</div>
         <ul>
-            <li v-for="item in infor">
-                <router-link :to="item.href">{{item.name}}</router-link>
+            <li class="left-item" v-for="item in infor">
+                <router-link :to="item.href"><i :class="item.lei"></i> {{item.name}}</router-link>
             </li>
         </ul>
-        <a href="/backManage.html"><i class="el-icon-setting"></i></a>
+        <li class="left-item"><a href="/backManage.html"><i class="el-icon-setting"></i> 设置</a></li>
     </div>
 </template>
 
 <script>
+
     export default {
         name: "left-nav",
         data (){
@@ -17,19 +21,24 @@
                 infor: [
                     {
                         href: '/',
-                        name: '首页'
+                        name: '首页',
+                        lei: 'el-icon-loading'
                     },{
                         href: '/specialtopic',
-                        name: '专题'
+                        name: '专题',
+                        lei: 'el-icon-goods'
                     },{
                         href: '/onfile',
-                        name: '归档'
+                        name: '归档',
+                        lei: 'el-icon-bell'
                     },{
                         href: '/link',
-                        name: '友链'
+                        name: '友链',
+                        lei: 'el-icon-share'
                     },{
                         href: '/about',
-                        name: '关于'
+                        name: '关于',
+                        lei: 'el-icon-service'
                     }
                 ]
             }
@@ -49,10 +58,38 @@
 </script>
 
 <style scoped>
-.left-nav{
-    width:20vw;
-    height:100vh;
-    float:left;
-    background-color: #ccc;
-}
+    .left-nav{
+        width:20vw;
+        height:100vh;
+        float:left;
+        background:url("../../static/img/my-bgcnav.png");
+    }
+    .left-header{
+        width: 130px;
+        border-radius: 50%;
+        margin: 30px auto 0;
+    }
+    .left-name{
+        font-size: 22px;
+        text-align: center;
+        color: #499ae2;
+        margin-top: 10px;
+        margin-bottom: 5px;
+    }
+    .left-infor{
+        color: #a0b3c3;
+        text-align: center;
+        font-size: 15px;
+    }
+    .left-item {
+        text-align: center;
+        margin-top: 10px;
+        /*color:#499ae2;*/
+    }
+    .left-item>a{
+        color:#499ae2;
+    }
+    .left-item>a:hover{
+        text-shadow: 1px 1px #e4bebe;
+    }
 </style>
--- Client/webpack.config.js
+++ Client/webpack.config.js
@@ -16,24 +16,24 @@ module.exports = {
             test: /\.vue$/,
             use: ['vue-loader']
         },
-            {
-                test: /\.js$/,
-                use: ['babel-loader'],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader', 'postcss-loader']
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|woff2|svg|svgz)(\?.+)?$/,
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        limit: 10000
-                    }
-                }]
-            }
+        {
+            test: /\.js$/,
+            use: ['babel-loader'],
+            exclude: /node_modules/
+        },
+        {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader', 'postcss-loader']
+        },
+        {
+            test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|woff2|svg|svgz)(\?.+)?$/,
+            use: [{
+                loader: 'url-loader',
+                options: {
+                    limit: 10000
+                }
+            }]
+        }
         ]
     },
   resolve: {
