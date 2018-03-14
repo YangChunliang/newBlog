df273a5685155649c4c307a6dd645cf03d75d567
yangchunliang <1711626079@qq.com>
2018-03-14 09:31:29 +0800

    修改文章列表页样式

--- Client/src/components/ArticleContent.vue
+++ Client/src/components/ArticleContent.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="content">
-        <ul>
-            <li v-for="item in helloData">{{ item.title }}</li>
-        </ul>
+        <div class="content-item" v-for="item in helloData">
+            <a class="c-title" href="javascript:;">{{ item.title }}</a>
+            <p>{{ item.content }}</p>
+            <a class="c-continue" href="javascript:;">继续阅读 >></a>
+        </div>
     </div>
 </template>
 
@@ -27,4 +29,36 @@
 </script>
 
 <style scoped>
+    .content{
+        padding: 40px 0 0 38px;
+        /*background-color: #ccc;*/
+        display: inline-block;
+    }
+    .content-item{
+        border-bottom: solid 1px #d9e2ea;
+        width:1015px;
+    }
+    .content-item>.c-title{
+        font-size: 28px;
+        font-weight: normal;
+        color: #333;
+        transition: color .3s;
+    }
+    .content-item>.c-title:hover{
+        color: #337ab7;
+    }
+    .content-item>p{
+        margin-top: 25px;
+        margin-bottom: 20px;
+    }
+    .c-continue{
+        display: inline-block;
+        color: #2479CC;
+        font-size: 15px;
+        transition: color .3s;
+        margin-bottom: 20px;
+    }
+    .c-continue:hover{
+        text-decoration: underline;
+    }
 </style>
