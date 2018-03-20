627302c76a52fdde8a70fd856c07e40dcc9a4a37
yangchunliang <1711626079@qq.com>
2018-03-20 14:11:28 +0800

    友链页增加内容

--- Client/src/components/MyLink.vue
+++ Client/src/components/MyLink.vue
@@ -1,12 +1,46 @@
 <template>
     <div class="my-link">
-        <div>友链：待整理</div>
+        <ul>
+            <li v-for="item in linkData">
+                <a :href="item.mLink" target="_blank">{{ item.name }}</a>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
     export default {
-        name: "mylink"
+        name: "mylink",
+        data(){
+            return{
+                linkData:[
+                    {
+                        name: '屈屈的博客',
+                        mLink: 'https://imququ.com/'
+                    },
+                    {
+                        name: '月影大大的博客',
+                        mLink: 'https://www.h5jun.com/'
+                    },
+                    {
+                        name: '奇舞周刊',
+                        mLink: 'https://weekly.75team.com/'
+                    },
+                    {
+                        name: '张鑫旭的空间',
+                        mLink: 'http://www.zhangxinxu.com/wordpress/'
+                    },
+                    {
+                        name: '前端乱炖',
+                        mLink: 'http://www.html-js.com/'
+                    },
+                    {
+                        name: 'w3cplus',
+                        mLink: 'https://www.w3cplus.com/'
+                    }
+                ]
+            }
+        }
     }
 </script>
 
@@ -16,5 +50,8 @@
         display: inline-block;
         width:1015px;
         margin-left: 20vw;
+        a{
+            color: #499ae2;
+        }
     }
 </style>
