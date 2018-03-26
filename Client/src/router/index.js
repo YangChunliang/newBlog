feaabd5a6063ded9b6140d6945d1dc41c35edbbb
yangchunliang <1711626079@qq.com>
2018-03-26 10:27:16 +0800

    更改模式

--- Client/src/router/index.js
+++ Client/src/router/index.js
@@ -10,7 +10,7 @@ import MyView from '../components/MyView.vue'
 Vue.use(Router);
 
 export default new Router({
-    mode: 'history',
+    //mode: 'history',
     routes: [
         {
             path: '/',
